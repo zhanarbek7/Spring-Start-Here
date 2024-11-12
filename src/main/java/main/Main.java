fcfb1b05b2fa4aeeb3b209bde1d61dcfbd353d02
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-12 16:52:25 -0600

    Done - Decoupling with Abstraction without Spring DI or IoC

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
 
+import models.Comment;
+import proxies.CommentNotificationProxy;
+import proxies.EmailCommentNotificationProxy;
+import repositories.CommentRepository;
+import repositories.DBCommentRepository;
+import services.CommentService;
+
 public class Main {
     public static void main(String[] args) {
-
+        CommentRepository commentRepository = new DBCommentRepository();
+        CommentNotificationProxy commentNotificationProxy = new EmailCommentNotificationProxy();
+        CommentService commentService = new CommentService(commentRepository, commentNotificationProxy);
+        Comment comment = new Comment();
+        comment.setText("This is a comment");
+        comment.setAuthor("John");
+        commentService.publishComment(comment);
     }
 }
