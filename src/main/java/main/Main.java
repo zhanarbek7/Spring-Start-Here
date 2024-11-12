43b8879f9639cfba4ce163ad1fa3f3dc3fa18cf9
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-11 18:48:34 -0600

    done

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,15 +4,24 @@ import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean(Parrot.class);
-        System.out.println(p);
 
+        Parrot parrot1 = new Parrot();
+        parrot1.setName("Kiki");
+
+
+        Supplier<Parrot> parrotSupplier1 = () -> parrot1;
+
+        context.registerBean("parrot",Parrot.class, parrotSupplier1,
+                bc -> bc.setPrimary(true));
+
+        Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
-        context.close();
+
     }
 }
--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -3,15 +3,9 @@ package main;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Parrot {
 
     private String name;
-
-    @PostConstruct
-    public void init() {
-        this.name = "Kiki";
-    }
 
     public String getName() {
         return name;
