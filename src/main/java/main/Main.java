e2b1873567afc88725e30358434fd6153829f3a1
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-12 16:53:47 -0600

    Done - Decoupling with Abstraction with Spring DI or IoC

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,21 +1,20 @@
 package main;
 
 
+import configuration.ProjectConfiguration;
 import models.Comment;
-import proxies.CommentNotificationProxy;
-import proxies.EmailCommentNotificationProxy;
-import repositories.CommentRepository;
-import repositories.DBCommentRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        CommentRepository commentRepository = new DBCommentRepository();
-        CommentNotificationProxy commentNotificationProxy = new EmailCommentNotificationProxy();
-        CommentService commentService = new CommentService(commentRepository, commentNotificationProxy);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfiguration.class);
         Comment comment = new Comment();
-        comment.setText("This is a comment");
         comment.setAuthor("John");
+        comment.setText("Hello world!");
+
+        CommentService commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
