a7fac0441d947c446e912c5db09983d44cec3ff1
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-12 19:33:51 -0600

    Listing 4.14 Using @Primary to mark the implementation as default

--- src/main/java/proxies/EmailCommentNotificationProxy.java
+++ src/main/java/proxies/EmailCommentNotificationProxy.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
