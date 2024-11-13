92e8e0cd540e564d77af32598f5b1cc119665030
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-13 14:48:30 -0600

    Proof: Singleton type beans, when requested from context return the same reference.

---
+++ src/main/java/configuration/ProjectConfig.java
@@ -0,0 +1,14 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import services.CommentService;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public CommentService commentService(){
+        return new CommentService();
+    }
+}
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,25 +1,5 @@
 package services;
 
-import models.Comment;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import proxies.CommentNotificationProxy;
-import repositories.CommentRepository;
-
-@Service
 public class CommentService {
-    private final CommentRepository commentRepository;
-    private final CommentNotificationProxy commentNotificationProxy;
-
-
-    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
-
-    public void publishComment(Comment comment) {
-        commentRepository.storeComment(comment);
-        commentNotificationProxy.sendComment(comment);
-    }
+    public int a = 5;
 }
