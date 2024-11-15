c8f66c0f55b82d8e9e5e68938f543a55f546cbf0
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-14 23:07:01 -0600

    * Now with Lazy instantiation, we can see that beans are created only when they are invoked or called.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,10 +2,13 @@ package main;
 
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+//        context.getBean(CommentService.class);
     }
 }
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,8 +1,10 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+@Lazy
 @Service
 public class CommentService {
     public CommentService() {
