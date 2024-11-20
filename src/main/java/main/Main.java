2778f112847645300afa14c4771f91b56101f824
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-11-20 16:49:32 -0600

    Updated the main For easier start for projects in the newer chapters.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,11 @@
-package main;
-
-import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(ProjectConfig.class);
-
-        CommentService commentService = context.getBean(CommentService.class);
-        for (String beanDefinitionName : context.getBeanDefinitionNames()) {
-            System.out.println(beanDefinitionName);
-        }
-
+                new AnnotationConfigApplicationContext();
     }
+
+
 }
